adb97ec3880c632b2fe26f186e73c2d7081a1974
ygursu <yasemingrs@gmail.com>
2024-03-11 22:34:08 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp134/Program.cs
@@ -0,0 +1,13 @@
+﻿double sinav = 0;
+Console.Write("Sınav notunu giriniz:");
+
+OkuSinav:
+sinav = Convert.ToDouble(Console.ReadLine());
+if (sinav < 0 || sinav > 100)
+{
+    Console.Write("Yanlış not bilgisi girdiniz.\nSınav notunuzu tekrardan giriniz:");
+    goto OkuSinav;
+}
+
+Console.WriteLine("Doğru not girişi yaptınız.");
+Console.ReadKey();
